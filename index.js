f8aaf0086a8bcd7465d6d1d2b068f64d2caf41e1
yulapshun <yulapshun@gmail.com>
2016-10-22 23:00:50 +0800

    Display click per second

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (function() {
+    
     window.onload = function() {
 
         document.body.addEventListener('touchmove', function(event) {
@@ -8,22 +9,29 @@
         var leftCount = 0;
         var rightCount = 0;
 
+        var startTime = null;
         var nextClick = 'left';
 
         var leftBtn = document.querySelector('#left');
         var rightBtn = document.querySelector('#right');
-        var display = document.querySelector('#display');
         var leftCountDiv = document.querySelector('#left_count');
         var rightCountDiv = document.querySelector('#right_count');
+        var cpsDiv = document.querySelector('#cps');
 
         leftBtn.addEventListener('touchstart', function() {
             if (nextClick !== 'left') {
                 return;
             }
             leftCount++;
+            if (!startTime) {
+                startTime = new Date();
+            }
+            var cps = (leftCount + rightCount) / ((new Date() - startTime) / 1000);
+
             leftCountDiv.innerHTML = leftCount;
             leftBtn.classList.remove('next-btn');
             rightBtn.classList.add('next-btn');
+            cpsDiv.innerHTML = cps;
 
             nextClick = 'right';
         });
@@ -32,9 +40,15 @@
                 return;
             }
             rightCount++;
+            if (!startTime) {
+                startTime = new Date();
+            }
+            var cps = (leftCount + rightCount) / ((new Date() - startTime) / 1000);
+
             rightCountDiv.innerHTML = rightCount;
             leftBtn.classList.add('next-btn');
             rightBtn.classList.remove('next-btn');
+            cpsDiv.innerHTML = cps;
 
             nextClick = 'left';
         });
