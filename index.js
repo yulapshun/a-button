f1d491a2a6bbb88219a6abf1fb087f79566407ca
yulapshun <yulapshun@gmail.com>
2016-11-14 21:48:59 +0800

    Implement time limit

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        time: 0,
-        clickCount: 0,
-        cps: 0,
-        startTime: null,
-        interval: null,
-        btnActive: false
-    },
-    methods: {
-        reset: function() {
-            this.time = 0;
-            this.clickCount = 0;
-            this.cps = 0;
-            this.startTime = null;
-            clearInterval(this.interval);
-            this.interval = null;
-            this.btnActive = false;
+(function() {
+    var TIME_LIMIT = 30;
+
+    var startTime = null;
+    var time = 0;
+    var interval = null;
+    var stopped = false;
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            time: 0,
+            clickCount: 0,
+            cps: 0,
+            btnActive: false
         },
-        onBtnDown: function(e) {
-            e.preventDefault();
-            this.btnActive = true;
-            this.clickCount++;
-            if (!this.startTime) {
-                var startTime = new Date();
-                this.startTime = startTime;
-                this.interval = setInterval((function() {
-                    this.time = Math.floor((new Date() - startTime) / 1000);
-                }).bind(this), 100);
+        methods: {
+            reset: function() {
+                this.stop();
+                startTime = null;
+                time = 0;
+                stopped = false;
+
+                this.time = 0;
+                this.clickCount = 0;
+                this.cps = 0;
+                this.btnActive = false;
+            },
+            stop: function() {
+                clearInterval(interval);
+                interval = null;
+                stopped = true;
+            },
+            onBtnDown: function(e) {
+                e.preventDefault();
+                this.btnActive = true;
+                if (!stopped) {
+                    this.clickCount++;
+                }
+                if (!startTime && !stopped) {
+                    startTime = new Date();
+                    interval = setInterval(this.tick, 100);
+                }
+                this.cps = time < 0.005 ? 0 : (this.clickCount * 1000 / time).toFixed(2);
+            },
+            onBtnUp: function() {
+                this.btnActive = false;
+            },
+            tick: function() {
+                time = new Date() - startTime;
+                this.time = Math.floor(time / 1000);
+                if (time >= TIME_LIMIT * 1000) {
+                    this.stop();
+                }
             }
-            var elapsedTime = ((new Date() - this.startTime) / 1000);
-            this.cps = elapsedTime < 0.005 ? 0 : ((this.clickCount + this.clickCount) / elapsedTime).toFixed(2);
-        },
-        onBtnUp: function() {
-            this.btnActive = false;
-        },
-        hihi: function() {
-            this.btnActive = true;
-        },
-        hihi2: function() {
-            this.btnActive = false;
         }
+    });
+
+
+    window.onload = function() {
+        document.body.addEventListener('touchmove', function(event) {
+            event.preventDefault();
+        }, false);
     }
-});
-
-
-window.onload = function() {
-    document.body.addEventListener('touchmove', function(event) {
-        event.preventDefault();
-    }, false);
-}
+})();
