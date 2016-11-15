912dd4f9f0203c06eed2a87b5259d503162ec25e
yulapshun <yulapshun@gmail.com>
2016-11-15 22:24:57 +0800

    Add css transform and transition to button

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('./', {cache: false});
+var fileServer = new static.Server('./', {cache: 1});
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
